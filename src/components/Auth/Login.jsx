7ce35bdee96aaf11c296ad8889b8a9c1204ace02
linarawwas <linamkraw2@gmail.com>
2023-10-14 13:34:24 +0300

    working on Log-in routing for admin and employee whether dashboard or home

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './Login.css'; // Import your CSS file
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate(); // Get the navigate function
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -16,13 +17,13 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     // Create an object with the user's email and password
     const credentials = {
       email,
       password,
     };
-  
+
     try {
       // Send a POST request to your authentication endpoint
       const response = await fetch('http://localhost:5000/api/auth/login', {
@@ -32,10 +33,21 @@ const Login = () => {
         },
         body: JSON.stringify(credentials),
       });
-  
+
       if (response.ok) {
-        // Authentication succeeded; you can redirect the user to another page or update the UI accordingly
-        console.log('Login successful');
+        // Authentication succeeded
+        // Retrieve the user's isAdmin status from the response or your authentication system
+        const data = await response.json();
+        const isAdmin = data.isAdmin;
+
+        // Redirect the user based on isAdmin status
+        if (isAdmin) {
+          // If the user is an admin, navigate to the Dashboard
+          navigate('/dashboard');
+        } else {
+          // If the user is not an admin, navigate to Home or another component
+          navigate('/home');
+        }
       } else {
         // Authentication failed; you can handle this by displaying an error message to the user
         console.error('Login failed');
@@ -44,7 +56,7 @@ const Login = () => {
       console.error('Login error:', error);
     }
   };
-  
+
   return (
     <div className="login-container">
       <h2>Login</h2>
