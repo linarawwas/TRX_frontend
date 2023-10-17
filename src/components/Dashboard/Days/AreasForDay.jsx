fed06d0329dbd7af28f9b9ef91f4b6c50d3a3145
linarawwas <linamkraw2@gmail.com>
2023-10-18 01:59:57 +0300

    updates for the area table

--- src/components/Dashboard/Days/AreasForDay.jsx
+++ src/components/Dashboard/Days/AreasForDay.jsx
@@ -1,50 +1,56 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"; // Import useParams from react-router-dom
+import { useParams ,Link} from "react-router-dom"; // Import useParams from react-router-dom
 
 export default function AreasForDay({ match }) {
-const { dayId } = useParams();
-  const [areas, setAreas] = useState([]);
-  const [loading, setLoading] = useState(true);
-const [dayName, setDayName]=useState('')
-  useEffect(() => {
-    // Fetch areas data for the specified day
-    fetch(`http://localhost:5000/api/areas/${dayId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setAreas(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching areas:', error);
-        setLoading(false);
-      });
-  }, [dayId]);
-  useEffect(() => {
-    // Fetch name of the the specified day
-    fetch(`http://localhost:5000/api/days/${dayId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setDayName(data.name);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching areas:', error);
-        setLoading(false);
-      });
-  }, [dayId]);
+    const { dayId } = useParams();
+    const [areas, setAreas] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [dayName, setDayName] = useState('')
+    useEffect(() => {
+        // Fetch areas data for the specified day
+        fetch(`http://localhost:5000/api/areas/${dayId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setAreas(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching areas:', error);
+                setLoading(false);
+            });
+    }, [dayId]);
+    useEffect(() => {
+        // Fetch name of the the specified day
+        fetch(`http://localhost:5000/api/days/${dayId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setDayName(data.name);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching areas:', error);
+                setLoading(false);
+            });
+    }, [dayId]);
 
-  return (
-    <div>
-      <h2>Areas for {dayName}</h2>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {areas.map((area) => (
-            <li key={area._id}>{area.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+    return (
+        <table className="days-table">
+            <thead>
+                <tr>
+                    <th>Areas for {dayName}</th>
+                </tr>
+            </thead>
+            {loading? (
+                <p>Loading ...</p>
+            ):(<tbody>
+                {areas.map((area) => (
+                    <tr key={area._id}>
+                        <td>
+                            <Link to={`/areas/${area._id}`}>{area.name}</Link>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>)}
+
+        </table>)
 }
