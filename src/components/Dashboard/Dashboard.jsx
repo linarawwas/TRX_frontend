b1c2b16c6ea5b4b120968f1505edd95a3e37b2cf
linarawwas <linamkraw2@gmail.com>
2023-10-18 15:59:43 +0300

    protected routes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,13 +3,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
 const Dashboard = () => {
+  const handleLogout = ()=>{
+  // Delete the token from local storage
+  localStorage.removeItem('token');
+
+  // Refresh the browser to clear the token and potentially navigate to a login page
+  window.location.reload();
+  }
   return (
     <div className="dashboard">
       <aside className="sidebar">
         <ul>
           <li>
             <Link to="/viewOrders">Orders</Link>
-          </li>          
+          </li>
           <li>
             <Link to="/recordOrder">Record Order</Link>
           </li>
@@ -21,7 +28,11 @@ const Dashboard = () => {
           </li>
           <li>
             <Link to="/customers">Customer</Link>
-          </li>
+          </li>       
+          <li>
+          <button onClick={handleLogout}>Logout
+          </button>
+          </li>          
         </ul>
       </aside>
       <main className="content">
