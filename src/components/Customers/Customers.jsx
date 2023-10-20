a102a1b157019dfcaeea458600475f2ea285405c
linarawwas <linamkraw2@gmail.com>
2023-10-20 18:15:44 +0300

    fixed customer and added pagination, still need to fix design

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -1,44 +1,102 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './Customers.css';
 
 export default function Customers({ match }) {
-    const [customers, setCustomers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        // Fetch areas data for the specified day
-        fetch(`http://localhost:5000/api/customers`)
-            .then((response) => response.json())
-            .then((data) => {
-                setCustomers(data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error fetching areas:', error);
-                setLoading(false);
-            });
-    }, [customers]);
-    return (
-        <table className="days-table">
-            <thead>
-            <tr>
-                    <th>All Customers</th>
-                </tr>                
-                <tr>
-                <th>name</th>
-                <th>phone</th>
-                <th>address</th>
-                </tr>
-            </thead>
-            {loading? (
-                <p>Loading ...</p>
-            ):(<tbody>
-                {customers.map((customer) => (
-                    <tr key={customer._id}>
-                        <td>{customer.name} </td>
-                        <td>{customer.phone} </td>
-                        <td>{customer.address} </td>
-                    </tr>
-                ))}
-            </tbody>)}
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
 
-        </table>)
+  useEffect(() => {
+    // Fetch customers data
+    fetch('http://localhost:5000/api/customers')
+      .then((response) => response.json())
+      .then((data) => {
+        setCustomers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching customers:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  // Number of records to display on each page
+  const recordsPerPage = 7;
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(customers.length / recordsPerPage);
+
+  // Function to handle page change
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  // Function to go to the previous page
+  const goToPreviousPage = () => {
+    if (currentPage > 0) {
+      handlePageChange(currentPage - 1);
+    }
+  };
+
+  // Function to go to the next page
+  const goToNextPage = () => {
+    if (currentPage < totalPages - 1) {
+      handlePageChange(currentPage + 1);
+    }
+  };
+
+  // Get the customers for the current page
+  const customersForPage = customers.slice(
+    currentPage * recordsPerPage,
+    (currentPage + 1) * recordsPerPage
+  );
+
+  return (
+    <div className="customers-body">
+      <h2>Customers</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <table className="days-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {customersForPage.map((customer) => (
+                <tr key={customer._id}>
+                  <td>{customer.name}</td>
+                  <td>{customer.phone}</td>
+                  <td>{customer.address}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {totalPages > 1 && (
+            <div className="pagination">
+              <div className="nav-arrow left" onClick={goToPreviousPage}>
+                &lt;
+              </div>
+              <Carousel showStatus={false} showArrows={false} showThumbs={false}>
+                {Array.from({ length: totalPages }, (_, i) => (
+                  <div key={i} onClick={() => handlePageChange(i)}>
+                    Page {i + 1}
+                  </div>
+                ))}
+              </Carousel>
+              <div className="nav-arrow right" onClick={goToNextPage}>
+                &gt;
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
