213d84795efc6623d9f3d64fc382c13cc59d1655
linarawwas <linamkraw2@gmail.com>
2023-10-28 13:36:07 +0300

    payment styling added for a userfriendly seemless experience

--- src/components/Orders/UpdateOrder/UpdateOrder.jsx
+++ src/components/Orders/UpdateOrder/UpdateOrder.jsx
@@ -6,7 +6,48 @@ function UpdateOrder() {
   const { orderId } = useParams();
   const [orderData, setOrderData] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  // State to hold payment data
+  const [paymentData, setPaymentData] = useState({
+    paymentAmount: '',
+    paymentCurrency: '', // Default currency, you can change this as needed
+    exchangeRateId: '6537789b6ed59ef09c18213d', // Default exchange rate, change as needed
+  });
+  const handlePaymentChange = (e) => {
+    const { name, value } = e.target;
+    setPaymentData({
+      ...paymentData,
+      [name]: value,
+    });
+  };
+      const handleAddPayment = async () => {
+        try {
+          const response = await fetch(`http://localhost:5000/api/orders/addPayment/${orderId}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(paymentData),
+          });
+      
+          if (response.ok) {
+            // Payment added successfully, fetch the updated order data
+            const updatedOrderResponse = await fetch(`http://localhost:5000/api/orders/${orderId}`);
+            if (updatedOrderResponse.ok) {
+              const updatedOrderData = await updatedOrderResponse.json();
+              setOrderData(updatedOrderData);
+              console.log('Payment added successfully');
+            } else {
+              // Handle errors when fetching the updated order data
+              console.error('Error fetching updated order data');
+            }
+          } else {
+            // Handle errors here
+            console.error('Error adding payment');
+          }
+        } catch (error) {
+          console.error('Error adding payment:', error);
+        }
+      };
   useEffect(() => {
     // Fetch the order details based on orderId
     fetch(`http://localhost:5000/api/orders/${orderId}`)
@@ -23,12 +64,11 @@ function UpdateOrder() {
 
   return (
     <div className="update-order-container">
-      <h1 className="update-order-title">Update Order</h1>
+      <h1 className="update-order-title">Billing Information:</h1>
       {loading ? (
         <p>Loading...</p>
       ) : orderData ? (
         <div>
-          <h2>Order Details</h2>
           <table className="order-details-table">
             <thead>
               <tr>
@@ -45,10 +85,62 @@ function UpdateOrder() {
               ))}
             </tbody>
           </table>
+          <div className="payment-form">
+  <h2 className="payment-heading">Add Payment</h2>
+  <form>
+    <div className="form-group">
+      <label htmlFor="paymentAmount" className="form-label">
+        Payment Amount
+      </label>
+      <input
+        type="number"
+        id="paymentAmount"
+        name="paymentAmount"
+        value={paymentData.paymentAmount}
+        onChange={handlePaymentChange}
+        className="form-input"
+      />
+    </div>
+    <div className="form-group">
+      <label htmlFor="paymentCurrency" className="form-label">
+        Payment Currency
+      </label>
+      <select
+        id="paymentCurrency"
+        name="paymentCurrency"
+        value={paymentData.paymentCurrency}
+        onChange={handlePaymentChange}
+        className="form-input"
+      >
+        <option value="LBP">LBP</option>
+        <option value="USD">USD</option>
+        {/* Add other currency options as needed */}
+      </select>
+    </div>
+    <div className="form-group">
+      <label htmlFor="exchangeRateId" className="form-label">
+        Exchange Rate ID
+      </label>
+      <input
+        type="text"
+        id="exchangeRateId"
+        name="exchangeRateId"
+        value={paymentData.exchangeRateId}
+        onChange={handlePaymentChange}
+        className="form-input"
+      />
+    </div>
+    <button type="button" onClick={handleAddPayment} className="add-payment-button">
+      Add Payment
+    </button>
+  </form>
+</div>
+
         </div>
       ) : (
         <p>Order not found</p>
       )}
+
     </div>
   );
 }
