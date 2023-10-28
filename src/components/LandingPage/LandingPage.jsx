4b744246f89059553002c7a9e8d45be13df4db68
linarawwas <linamkraw2@gmail.com>
2023-10-28 15:29:34 +0300

    made a lnading page suitable

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,16 +5,28 @@ function LandingPage() {
   return (
     <div className="landing-page">
       <header className="landing-header">
-        <h1>Welcome to Your App</h1>
-        <p>Your catchphrase or brief introduction here</p>
+        <h1>Welcome to OrdrTrkr</h1>
+        <p>Your Order Management and Delivery Tracking Solution</p>
       </header>
       <section className="landing-content">
         <p>
-          This is the landing page content. You can provide a warm welcome message, features overview, or any other information you want to showcase.
+          OrdrTrkr is your all-in-one platform for streamlining order management, tracking deliveries, and ensuring a seamless customer experience. With OrdrTrkr, you can easily manage orders, monitor deliveries in real-time, and delight your customers.
+        </p>
+        <p>
+          Key Features:
+          <ul>
+            <li>Effortless Order Management</li>
+            <li>Real-time Delivery Tracking</li>
+            <li>Customer-Focused Approach</li>
+            <li>Easy Payment Integration</li>
+          </ul>
+        </p>
+        <p>
+          Join us on a journey to simplify your business operations and elevate your customer satisfaction. Experience the future of order and delivery management with OrdrTrkr!
         </p>
       </section>
       <footer className="landing-footer">
-        <p>&copy; 2023 Your Company</p>
+        <p>&copy; 2023 OrdrTrkr</p>
       </footer>
     </div>
   );
