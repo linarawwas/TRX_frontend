1009f7fca62f5655f65fe124ae897dcf5a963e3a
linarawwas <linamkraw2@gmail.com>
2023-12-01 21:41:02 +0200

    saved redux states in localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { legacy_createStore as createStore } from 'redux'
 import rootReducer from './rootReducer';
 
+// Load the state from localStorage when initializing the Redux store
+const savedState = localStorage.getItem('reduxState');
+const initialState = savedState ? JSON.parse(savedState) : {};
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    initialState
 );
-
+store.subscribe(() => {
+    const state = store.getState();
+    localStorage.setItem('reduxState', JSON.stringify(state));
+});
 export default store;
