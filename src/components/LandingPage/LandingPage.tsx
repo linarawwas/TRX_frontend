f5adfbf2f011030d35102b389100c48bd6f49577
linarawwas <linamkraw2@gmail.com>
2023-12-14 20:44:55 +0200

    landing page and branding

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -4,35 +4,28 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 const LandingPage: React.FC = () => {
   const name = useSelector((state: RootState) => state.user.username);
-
-  return (
-    <div className="landing-page">
-      <header className="landing-header">
-        <h1>Hello {name}, welcome to OrdrTrkr!!</h1>
-        <p>Your Order Management and Delivery Tracking Solution</p>
-      </header>
-      <section className="landing-content">
-        <p>
-          OrdrTrkr is your all-in-one platform for streamlining order management, tracking deliveries, and ensuring a seamless customer experience. With OrdrTrkr, you can easily manage orders, monitor deliveries in real-time, and delight your customers.
-        </p>
-        <div>
-          Key Features:
+    return (
+      <div className="landing-page">
+        <div className="hero-section">
+          <h1>Welcome to TRX, {name}</h1>
+          <p>A powerful inventory management system</p>
+          <button className="get-started-button">Get Started</button>
+        </div>
+        <div className="feature-section">
+          <h2>Key Features</h2>
           <ul>
-            <li>Effortless Order Management</li>
-            <li>Real-time Delivery Tracking</li>
-            <li>Customer-Focused Approach</li>
-            <li>Easy Payment Integration</li>
+            <li>Track your inventory effortlessly</li>
+            <li>Real-time updates and notifications</li>
+            <li>Advanced reporting and analytics</li>
           </ul>
         </div>
-        <p>
-          Join us on a journey to simplify your business operations and elevate your customer satisfaction. Experience the future of order and delivery management with OrdrTrkr!
-        </p>
-      </section>
-      <footer className="landing-footer">
-        <p>&copy; 2023 OrdrTrkr</p>
-      </footer>
-    </div>
-  );
-}
-
-export default LandingPage;
+        <div className="footer">
+          <p>&copy; 2023 TRX. All Rights Reserved.</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default LandingPage;
+  
+  
