83e6c0a0df3ec9dfb8eeebad37bd63436813d9c2
linarawwas <linamkraw2@gmail.com>
2024-01-02 15:30:09 +0200

    order receipt design changed

--- src/components/Orders/UpdateOrder/OrderReceipt/OrderReceipt.tsx
+++ src/components/Orders/UpdateOrder/OrderReceipt/OrderReceipt.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import SpinLoader from '../../../UI reusables/SpinLoader/SpinLoader';
-
+import './OrderReceipt.css'
 interface OrderReceiptProps {
     orderData: any; // Change 'any' to the type of your orderData if available
     loading: boolean;
 }
 
 const OrderReceipt: React.FC<OrderReceiptProps> = ({ orderData, loading }: OrderReceiptProps) => {
+    const formatTimestamp = (timestamp: string) => {
+        const date = new Date(timestamp);
+        // Adjust the received timestamp by subtracting 2 hours for the Beirut timezone
+        date.setHours(date.getHours() - 2);
+
+        const options: Intl.DateTimeFormatOptions = {
+            timeZone: 'Asia/Beirut',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true // Set to true for 12-hour format
+        };
+
+        return date.toLocaleString('en-US', options);
+    };
+
+
     return (
         <>
             {loading ? (
                 <SpinLoader />
             ) : orderData ? (
+                <div className='order-receipt'>
+
                 <div className="receipt-details">
+                    <div className="receipt-detail">
+                        <p className="detail-name">Date: </p>
+                        <p className="detail-value">{formatTimestamp(orderData?.timestamp)}</p>
+                    </div>
                     <div className="receipt-detail">
                         <p className="detail-name">Delivered: </p>
                         <p className="detail-value">{orderData?.delivered}</p>
@@ -42,6 +68,8 @@ const OrderReceipt: React.FC<OrderReceiptProps> = ({ orderData, loading }: Order
                         <p className="detail-value">{orderData?.total.toFixed(2)}</p>
                     </div>
                 </div>
+                </div>
+
             ) : (
                 <p>Order not found</p>
             )}
