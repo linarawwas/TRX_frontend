b3d790a040aa9a56efff22ed5365683d754f5049
linarawwas <linamkraw2@gmail.com>
2024-01-07 11:46:21 +0200

    feature list for employee

--- src/components/AsideMenu/AsideMenu/AsideMenuEmployee.tsx
+++ src/components/AsideMenu/AsideMenu/AsideMenuEmployee.tsx
@@ -62,12 +62,6 @@ const AsideMenuEmployee: React.FC = () => {
             ) : (
               <li>Please Start Shipment</li>
             )}
-            {/* <li>
-              <Link to="/addExpenses" className='sidebar-link' onClick={toggleMenu}>Add Expenses</Link>
-            </li>
-            <li>
-              <Link to="/addProfits" className='sidebar-link' onClick={toggleMenu}>Add Profits</Link>
-            </li> */}
             <li>
               <button className='logout-button' onClick={handleLogout}>Logout</button>
             </li>
--- src/components/LandingPage/FeatureSection.jsx
+++ src/components/LandingPage/FeatureSection.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import { clearShipmentInfo } from '../../redux/Shipment/action';
 import { Link } from 'react-router-dom';
 import StartShipment from '../EmployeeComponents/StartShipment/StartShipment';
+import { FaTimes } from 'react-icons/fa';
 const FeatureSection = () => {
     const dispatch = useDispatch();
     const [showProfitsForm, setShowProfitsForm] = useState(false)
@@ -17,9 +18,9 @@ const FeatureSection = () => {
             <div className="feature-section">
                 <h2>Key Features</h2>
                 <ul>
-                    <li className='show-form-li' onClick={() => { setShowShipmentForm(!showShipmentForm) }}>Start a new shipment</li>
-                    <li className='show-form-li' onClick={() => { setShowProfitsForm(!showProfitsForm) }}>Add Extra Profits</li>
-                    <li className='show-form-li' onClick={() => { setShowExpensesForm(!showExpensesForm) }}>Add Expenses</li>
+                    <li className='show-form-li' onClick={() => { setShowShipmentForm(!showShipmentForm) }}>{showShipmentForm ? <FaTimes /> : "Start a new shipment"}</li>
+                    <li className='show-form-li' onClick={() => { setShowProfitsForm(!showProfitsForm) }}>{showProfitsForm ? <FaTimes /> : "Add Extra Profits"}</li>
+                    <li className='show-form-li' onClick={() => { setShowExpensesForm(!showExpensesForm) }}>{showExpensesForm ? <FaTimes /> : "Add Expenses"}</li>
                 </ul>
             </div>
             {showProfitsForm && <AddProfits />}
