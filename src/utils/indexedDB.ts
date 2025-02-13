7487db2b7373518d166410da427f6ee6e76d1d80
linarawwas <linamkraw2@gmail.com>
2025-02-13 16:42:51 +0200

    saving areas in offlineDB indexedDB

--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -1,27 +1,41 @@
 import { openDB } from 'idb';
 
-const DB_NAME = 'AreasDB';
-const STORE_NAME = 'areas';
+const DB_NAME = 'OfflineDB'; // Use a single database
+const AREA_STORE = 'areas';
+const CUSTOMER_STORE = 'customers';
 
 export async function initDB() {
   return openDB(DB_NAME, 1, {
     upgrade(db) {
-      if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME, { keyPath: '_id' });
+      if (!db.objectStoreNames.contains(AREA_STORE)) {
+        db.createObjectStore(AREA_STORE, { keyPath: '_id' });
+      }
+      if (!db.objectStoreNames.contains(CUSTOMER_STORE)) {
+        db.createObjectStore(CUSTOMER_STORE, { keyPath: '_id' });
       }
     },
   });
 }
 
-export async function saveAreas(areas: any[]) {
+// Generic function to save data
+async function saveData(storeName: string, data: any[]) {
   const db = await initDB();
-  const tx = db.transaction(STORE_NAME, 'readwrite');
-  const store = tx.objectStore(STORE_NAME);
-  areas.forEach((area) => store.put(area));
+  const tx = db.transaction(storeName, 'readwrite');
+  const store = tx.objectStore(storeName);
+  data.forEach((item) => store.put(item));
   await tx.done;
 }
 
-export async function getAreas() {
+// Generic function to get data
+async function getData(storeName: string) {
   const db = await initDB();
-  return db.getAll(STORE_NAME);
+  return db.getAll(storeName);
 }
+
+// ---- Areas Functions ----
+export const saveAreas = (areas: any[]) => saveData(AREA_STORE, areas);
+export const getAreas = () => getData(AREA_STORE);
+
+// ---- Customers Functions ----
+export const saveCustomers = (customers: any[]) => saveData(CUSTOMER_STORE, customers);
+export const getCustomers = () => getData(CUSTOMER_STORE);
