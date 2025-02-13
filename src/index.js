a3c4bda0f7dbeb6c8f6876c825a6731f9ed0964d
linarawwas <linamkraw2@gmail.com>
2025-02-13 17:38:59 +0200

    implemented redux persist to save redux state offline

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { PersistGate } from 'redux-persist/integration/react'; // This is the wrapper for Persist
+import { store , persistor} from "./redux/store.ts";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
+    </PersistGate>    </Provider>
   </React.StrictMode>
 );
 
