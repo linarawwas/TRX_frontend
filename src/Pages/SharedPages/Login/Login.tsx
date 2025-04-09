10d75e1e95c59fbbaca13a1a8567420f6ad8e0fb
linarawwas <linamkraw2@gmail.com>
2025-04-09 18:35:56 +0300

    enhanced login ux

--- src/Pages/SharedPages/Login/Login.tsx
+++ src/Pages/SharedPages/Login/Login.tsx
@@ -30,13 +30,21 @@ export default function Login(): JSX.Element {
     };
   }, []);
   return (
-    <div className='login-body'>
+   <div className='login-container'>
+<div className='white-bg'>
+  <div className='blue-box'></div>
+</div>
+<div className='blue-bg'>
+  <div className='white-box'></div>
+</div>
+   </div>
+  );
+};
+{/* <div className='login-body'>
       <h1 className="login-title">Login</h1>
       <div className="login-container">
         <ToastContainer position="top-right" autoClose={1000} />
         <h2 className='login-title'>{showLoginForm ? "" : ""}</h2>
         {showLoginForm && <LoginForm />}
       </div>
-  </div>
-  );
-};
+  </div> */}
