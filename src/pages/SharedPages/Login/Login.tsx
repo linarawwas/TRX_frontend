5d87522e49de5d338f3d36b8a17b33e0adb7e786
linarawwas <linamkraw2@gmail.com>
2026-03-24 23:34:33 +0200

    login wrapper fixed

--- src/pages/SharedPages/Login/Login.tsx
+++ src/pages/SharedPages/Login/Login.tsx
@@ -1,10 +1,18 @@
 import "./Login.css";
+import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LoginForm from "./LoginForm";
 import logo from "../../../images/logo.jpeg";
 
 export default function Login(): JSX.Element {
+  useEffect(() => {
+    document.body.classList.add("login-page");
+    return () => {
+      document.body.classList.remove("login-page");
+    };
+  }, []);
+
   return (
     <div className="login-wrapper">
       <ToastContainer position="top-right" autoClose={1000} />
